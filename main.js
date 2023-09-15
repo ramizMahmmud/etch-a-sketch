25ea08d6c00159550841fea228182c836b71d79b
ramizMahmmud <89058495+ramizMahmmud@users.noreply.github.com>
2023-09-15 15:56:10 +0600

    Change background color
    by clicking any grid element one can change background color to black

--- main.js
+++ main.js
@@ -5,5 +5,15 @@ for(let i = 1; i<=16*16; i++){
     myDiv.classList.add('inner-div');
     container.appendChild(myDiv);
 }
+function changeColor(e){
+    e.target.style.background = 'black';
+
+}
+const divs = document.querySelectorAll('.inner-div');
+
+divs.forEach((div) => {
+    div.addEventListener('click',changeColor);
+});
+
 
 
