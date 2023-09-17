c751ff2fd38d018e96226aeb1794d7d92fcbdff9
ramizMahmmud <89058495+ramizMahmmud@users.noreply.github.com>
2023-09-17 20:53:08 +0600

    Add user input system
    a button is added. user can click that button and change grid size

--- main.js
+++ main.js
@@ -1,19 +1,52 @@
 const container = document.querySelector('#container');
-for(let i = 1; i<=16*16; i++){
+const btn = document.getElementById('grid-btn');
 
-    const myDiv = document.createElement('div');
-    myDiv.classList.add('inner-div');
-    container.appendChild(myDiv);
+// let gridNumber = 50;
+
+
+function creatGrid(gridNumber){
+
+    let gridDimension = 640/gridNumber;
+    for(let i = 1; i<=gridNumber*gridNumber; i++){
+        
+        const myDiv = document.createElement('div');
+        myDiv.classList.add('inner-div');
+        myDiv.setAttribute('style',`height: ${gridDimension}px; width: ${gridDimension}px;`);
+        container.appendChild(myDiv);
+    }
 }
-function changeColor(e){
-    e.target.style.background = 'black';
 
+function deletDiv(){
+    let divs = container.querySelectorAll('.inner-div');    
+    divs.forEach((x) => {
+        container.removeChild(x);
+    });
+    
 }
-const divs = document.querySelectorAll('.inner-div');
 
-divs.forEach((div) => {
-    div.addEventListener('click',changeColor);
-});
+function draw() {
+    
+    const divs = document.querySelectorAll('.inner-div');
+    
+    function changeColor(e){
+        e.target.style.background = 'black';
+        
+    }
+    
+    divs.forEach((div) => {
+        div.addEventListener('mouseover',changeColor);
+    });
+}
+function getUserInput(){
+    let newGridNumber = parseInt(prompt('Enter grid number(less than 100)'));
+    deletDiv(); 
+    creatGrid(newGridNumber);
+    draw();
+}
 
+creatGrid(50);
+draw();
+
+btn.addEventListener('click',getUserInput);
 
 
